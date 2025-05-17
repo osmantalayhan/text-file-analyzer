dc0dfe57b03a681715ad1f21f7218bc1f08b09f1
Osman Talayhan <143598353+osmantalayhan@users.noreply.github.com>
2025-05-17 15:12:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,18 +6,14 @@ from analyzer import count_word
 from analyzer import word_by_sentence
 from analyzer import sentence_operations
 
-
 with open('overview.txt', 'r', encoding='utf-8') as file:
     text = file.read()
-
+    
 print(sentiment_analysis(text))
 sentences = find_sentence_number(text)
 print(average_sentence_length(sentences))
 print(find_max_min_word(sentences))
 print(count_word(sentences), len(count_word(sentences)))
-
-
 all_counts = word_by_sentence(sentences)
 print(all_counts)
-
 print(sentence_operations(all_counts))
